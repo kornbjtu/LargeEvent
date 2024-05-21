3384c654cfd491eefdd7bd0d7f04c85cb522e558
GaryWuBigBear <1205169294@qq.com>
2024-05-21 16:27:32 +0200

    as

--- Vis.py
+++ Vis.py
@@ -1,33 +1,41 @@
 import cv2
 import numpy as np
+from playground import *
 
 class Visualizor:
     def __init__(self):
-        self.canvas = np.zeros((120, 120, 3), dtype=np.uint8)
+        self.canvas = np.zeros((700, 700, 3), dtype=np.uint8)
         self.fps = 24
         self.truck_color = (0, 0, 255)
-        self.truck_radius = 2
-
-    def draw_truck(self, x: int, y: int):
-        canvas_x = int(x + 60)
-        canvas_y = int(y + 60)
-        cv2.circle(self.canvas, (canvas_x, canvas_y), self.truck_radius, self.truck_color, -1)
+        self.truck_radius = 5
 
     def update_canvas(self):
         cv2.imshow('Canvas', self.canvas)
         key = cv2.waitKey(int(1000/self.fps))
         return key  # 返回按下的键的ASCII码
     
-    def draw(self, x: int, y: int):
-        self.draw_truck(x, y)
-        
-        while True:  # 无限循环
-            key = self.update_canvas()
-            if key != -1:  # 如果用户按下了键，跳出循环
-                break
-                cv2.destroyAllWindows()
+    def trans(self, x: int, y: int):    #coordinate transfer
+        canvas_x = int(5*(x + 60))
+        canvas_y = int(700-5*(y + 60))
+        cv2.circle(self.canvas, (canvas_x, canvas_y), self.truck_radius, self.truck_color, -1)
 
-visualizor = Visualizor()
-visualizor.draw(10, 20)
+    def animate_trucks(self):
+        for truck in all_trucks:
+            self.trans(truck.location.x, truck.location.y)
+        key = self.update_canvas()
+        if key != -1:  # 如果用户按下了键，跳出循环
+            cv2.destroyAllWindows()
+        self.canvas.fill(0)  # 清空画布以便于下一帧
+        cv2.waitKey(0)  # 动画结束后，窗口将保持打开状态，直到用户按下任意键
+        cv2.destroyAllWindows()
+
+   
+           
+    def draw(self):
+        self.animate_trucks()
+    
+   
 
 
+vis = Visualizor()
+vis.draw()

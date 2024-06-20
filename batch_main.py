cf2ca387feb33dddd4452a239f2c858ff71142b8
liheng423 <liguoheng423@icloud.com>
2024-06-20 15:19:45 +0200

    updates

---
+++ batch_main.py
@@ -0,0 +1,11 @@
+
+## 1. read default file, find out the difference
+
+## 2. a file storing scenarios & configurations
+
+## 3. read this file
+
+
+## 3. for seed for scenario
+
+##      4. revoke batch_main's main programe -> return csv with filename of "scenario+seed+output.csv"
